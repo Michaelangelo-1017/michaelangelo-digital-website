5295d6c4ad47b46aae3cf96149fb0f4fdd8a84b2
Michaelangelo <jagbodyke01@gmail.com>
2026-05-17 19:57:34 +0100

    Add icons data to layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -46,6 +46,11 @@ export const metadata: Metadata = {
     index: true,
     follow: true,
   },
+  icons: {
+    icon: '/favicon.ico',
+    shortcut: '/favicon.ico',
+    apple: '/favicon.ico',
+  },
 };
 
 export default function RootLayout({
